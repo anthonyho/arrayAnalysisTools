49d2673fc237ebdf78332db71638a55bf6c41098
Anthony Ho <anthony.hyho@gmail.com>
2015-01-26 18:52:09 -0800

    Changes python styles for summarizeCPfluor.py

--- quantTools/summarizeCPfluor.py
+++ quantTools/summarizeCPfluor.py
@@ -17,30 +17,30 @@ import ahplot
 
 
 ## Print analysis summaries
-def printSummary(text,number,totalNumber):
-    print text, str(number), "("+str(round(float(number)/totalNumber*100,2))+"%)"
+def printSummary(text, number, totalNumber):
+    print text, str(number), "("+str(round(float(number)/totalNumber*100, 2))+"%)"
     return
 
 
 ## Show histograms
-def showHistograms(plotTitle,data1,xlabel1,data2,xlabel2,data3,xlabel3):
+def showHistograms(plotTitle, data1, xlabel1, data2, xlabel2, data3, xlabel3):
 
-    fig = plt.figure(figsize=(24,9))
+    fig = plt.figure(figsize=(24, 9))
     numBins = 50
 
     data = [data1, data2, data3]
     xLabels = [xlabel1, xlabel2, xlabel3]
     ax = []
 
-    for i in range(0,3):
-        ax.append(fig.add_subplot(1,3,i+1))
+    for i in range(0, 3):
+        ax.append(fig.add_subplot(1, 3, i+1))
         ax[i].hist(data[i],numBins,color='#3F5D7D',alpha=0.8)
 
         ax[i].set_xlabel(xLabels[i])
         ahplot.makepretty(ax[i])
 
     ax[0].set_ylabel("Count")
-    ax[1].set_title(plotTitle,y=1.05)
+    ax[1].set_title(plotTitle, y=1.05)
     
     return
 
@@ -48,12 +48,12 @@ def showHistograms(plotTitle,data1,xlabel1,data2,xlabel2,data3,xlabel3):
 def main():
 
     ## Get options and arguments from command line
-    parser = argparse.ArgumentParser(description="Summarize a CPfluor file")
-    parser.add_argument("-p","--plot", action='store_true', default=False, help="option to plot figures")
-    parser.add_argument("CPfluorPath", help="CPfluor file to be analyzed")
-    parser.add_argument("filterName", help="name of the filter to be used")
-    parser.add_argument("filteredFilePath", help="location of filtered tile file")
-    parser.add_argument("outputDir", help="output directory to store the analyzed data and figure")
+    parser = argparse.ArgumentParser(description="summarize a CPfluor file")
+    parser.add_argument('-p', '--plot', action='store_true', default=False, help="option to plot figures")
+    parser.add_argument('CPfluorPath', help="CPfluor file to be analyzed")
+    parser.add_argument('filterName', help="name of the filter to be used")
+    parser.add_argument('filteredFilePath', help="location of filtered tile file")
+    parser.add_argument('outputDir', help="output directory to store the analyzed data and figure")
     args = parser.parse_args()
 
     ## Initialization
@@ -70,12 +70,12 @@ def main():
     print "Analyzing "+CPfluorBaseName+"..."
 
     ## Output files
-    outputFilePath = os.path.join(args.outputDir,CPfluorBaseName+".sum")
-    figurePath = os.path.join(args.outputDir,CPfluorBaseName+".sum.eps")
+    outputFilePath = os.path.join(args.outputDir, CPfluorBaseName+".sum")
+    figurePath = os.path.join(args.outputDir, CPfluorBaseName+".sum.eps")
 
     ## Go through the CPfluor file
-    with open(args.CPfluorPath, "r") as r1, open(args.filteredFilePath, "r") as r2, open(outputFilePath, "w") as w:
-        for line1, line2 in izip(r1,r2):
+    with open(args.CPfluorPath, 'r') as r1, open(args.filteredFilePath, 'r') as r2, open(outputFilePath, 'w') as w:
+        for line1, line2 in izip(r1, r2):
             
             # Read the CPfluor file
             CPfluorLine = line1.rstrip().split(':')
@@ -90,7 +90,7 @@ def main():
 
                 numSeqs += 1
                 
-                if fitted =="1":
+                if fitted == '1':
 
                     numFittedSeqs += 1
                     
@@ -102,7 +102,7 @@ def main():
                     intFluor = str(twoPi*float(amp)*float(sigma)**2)
                     
                     # Write to output file
-                    w.write(intFluor+"\t"+amp+"\t"+sigma+"\n")
+                    w.write(intFluor+'\t'+amp+'\t'+sigma+'\n')
                     
                     # Append to lists
                     allIntFluor.append(intFluor)
@@ -110,15 +110,15 @@ def main():
                     allSigma.append(sigma)
     
     ## Print summaries
-    printSummary("  Number of clusters with the correct filter:",numSeqs,numSeqs)
-    printSummary("  Number of clusters fitted:",numFittedSeqs,numSeqs)
+    printSummary("  Number of clusters with the correct filter:", numSeqs, numSeqs)
+    printSummary("  Number of clusters fitted:", numFittedSeqs, numSeqs)
 
     ## Plot and save histograms
     showHistograms(CPfluorBaseName+" (fitted = "+str(numFittedSeqs)+"/"+str(numSeqs)+" = "
                    +str(round(float(numFittedSeqs)/numSeqs*100,2))+"%)", 
-                   np.array(map(float,allIntFluor)),"Integrated fluorescence", 
-                   np.array(map(float,allAmp)),"Amplitude", 
-                   np.array(map(float,allSigma)),"Sigma")
+                   np.array(map(float,allIntFluor)), "Integrated fluorescence", 
+                   np.array(map(float,allAmp)), "Amplitude", 
+                   np.array(map(float,allSigma)), "Sigma")
     plt.savefig(figurePath, bbox_inches='tight')
 
     ## Show the histograms if requested
