8bf2b36f95ef1aea3114f623a6064d8ac51566ca
Anthony Ho <anthony.hyho@gmail.com>
2015-01-31 02:20:22 -0800

    alignAnnotate.py is now mostly functional

--- pythonLib/seqlib.py
+++ pythonLib/seqlib.py
@@ -3,22 +3,69 @@
 # Alignemnt and annotation tools
 #
 # Anthony Ho, ahho@stanford.edu, 1/28/2015
-# Last update 1/28/2015
+# Last update 1/31/2015
 
 
 ## Import libraries
+#import pandas as pd
+import subprocess
+import numpy as np
 
+## Parse fasta sequences from string, separated by \n
+def parseFastaSeqsAsString(fastaStr, IDchar='>'):
+    return [''.join(record.split('\n')[1:]) for record in fastaStr.split(IDchar)[1:]]
 
-## Hamming distance calculator, assuming equal length
-def hammingDistance(s1,s2):
+## Compute Hamming distance between two sequences, assuming equal length
+def hamming(s1, s2):
     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
-## Stupid way to find the first (and only) deletion
-def findIndel(q,refU):
-    for i in range(0,len(refU)):
-        if q[i] != refU[i]:
-            bail = hammingDistance(q[i+1:i+9],refU[i+2:i+10]) <= 2
-            if bail:
-                return i+1
-    return -1000
+## Find mismatches between two sequences, assuming equal length
+def findMismatches(query, reference, startPos=1, refPos=[]):
+    # User-defined nucleotide positions
+    if not refPos:
+        realRefPos = range(0,len(query))
+    else:
+        realRefPos = np.cumsum(refPos)-1
+    # Return list of mismatches
+    return [chR+str(startPos+realRefPos[i])+chQ for i, (chQ, chR) in enumerate(zip(query, reference)) if chQ != chR]
 
+## Find mismatches and indels between two aligned sequences (i.e. equal length after gapping)
+def findMismatchesAndIndels(query, reference, startPos=1, refPos=[]):
+
+    # User-defined nucleotide positions
+    if not refPos:
+        realRefPos = range(0,len(query))
+    else:
+        realRefPos = np.cumsum(refPos)-1
+
+    # Find insertions
+    # Get all the insertions in the unadjusted coordinates
+    rawTupleListIn = [(i, chQ) for i, (chQ, chR) in enumerate(zip(query, reference)) if chR == '-']
+    # Adjust to the correct coordinates, map to the user-specific position, and output list of annotations
+    listIn = ['+'+str(startPos+realRefPos[i-j])+chQ for j, (i, chQ) in enumerate(rawTupleListIn)]
+    
+    # Shorten the sequences to get rid of the gaps in the reference sequences and the corresponding 
+    # nucleotide in the query sequence
+    rawListInPos = [i for i, chQ in rawTupleListIn]
+    shortQuery = ''.join([ch for i, ch in enumerate(query) if i not in rawListInPos])
+    shortReference = ''.join([ch for i, ch in enumerate(reference) if i not in rawListInPos])
+    
+    # Find mismatches and deletions
+    listMM = [chR+str(startPos+realRefPos[i])+chQ for i, (chQ, chR) in enumerate(zip(shortQuery, shortReference)) if chQ != chR and chQ != '-']
+    listDel = [chR+str(startPos+realRefPos[i])+'x' for i, (chQ, chR) in enumerate(zip(shortQuery, shortReference)) if chQ == '-']
+
+    return listMM, listIn, listDel
+
+## Align a query sequence to a reference sequence using EMBOSS's Needleman-Wunsch program
+## Note: it assumes the EMBOSS needle program is installed and only works on Unix/Mac-based OS 
+def alignEmbossNeedle(query, reference, gapopen=10, gapextend=0.5):
+    cmdBash = ' /bin/bash -c '
+    cmdAlign = ' needle -asequence <(echo ' + query + ') -bsequence <(echo ' + reference + ')' 
+    optionsAlign = ' -gapopen ' + str(gapopen) + ' -gapextend ' + str(gapextend) + ' -aformat fasta -stdout -auto'
+    fullCmd = cmdBash + '\"' + cmdAlign + optionsAlign + '\"'
+    process = subprocess.Popen(fullCmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    process.wait()
+    [stdout, stderr] = process.communicate()
+    if stderr:
+        print 'Needleman-Wunsch alignement returns with error'
+    return parseFastaSeqsAsString(stdout)
--- seqTools/alignAnnotate.py
+++ seqTools/alignAnnotate.py
@@ -12,31 +12,133 @@ import os, sys
 import argparse
 import pandas as pd
 import numpy as np
-import alignLibs
+from Bio import SeqIO
+import seqlib
+
+
+## Make annotation label given alignment information
+def makeAnnotation(label, numMM, numIn, numDel, listMM, listIn, listDel):
+    csMM = ','.join(listMM)
+    csIn = ','.join(listIn)
+    csDel = ','.join(listDel)
+    listAnnotation = [label, str(numMM), str(numIn), str(numDel), csMM, csIn, csDel]
+    annotation = ':'.join(listAnnotation)
+    return annotation
+
+
+## Align and annotate a query sequence against a set of reference sequences if ignoring indels completely
+def alignAnnotateEachSeqMMonly(querySeq, refSeqsDict, startPos, refPosDict, MMcutoff):
+
+    # Make a dictionary of HD of the query against all references that have the same length as query 
+    hamDict = {refSeqID: seqlib.hamming(querySeq, refSeq) for refSeqID, refSeq in refSeqsDict.items() if len(querySeq) == len(refSeq)}
+
+    # If any of the references has the same length as query:
+    if hamDict:
+
+        # Find the (IDs of) references that have the smallest hamming distance (i.e. most similar to reference)
+        minHam = min(hamDict.values())
+        minHamRefIDs = [refSeqID for refSeqID, refSeqHam in hamDict.items() if refSeqHam == minHam]
+        
+        if minHam <= MMcutoff:
+            # Output warning if more than one reference has the smallest hamming distance
+            if len(minHamRefIDs) > 1:
+                print "Warning! Multiple reference sequences have the same Hamming distance with the query sequence"
+
+            # Get list of mismatches
+            matchID = minHamRefIDs[0]
+            listMM = seqlib.findMismatches(querySeq, refSeqsDict[matchID], startPos, refPosDict[matchID])
+            return makeAnnotation(matchID, minHam, 0, 0, listMM, [], [])
+
+        # Report N/A if # MM > MMcutoff
+        else:
+            return makeAnnotation('NA', np.nan, np.nan, np.nan, [], [], [])
+
+    # Report N/A if indels
+    else:
+        return makeAnnotation('NA', np.nan, np.nan, np.nan, [], [], [])
+
+
+## Align and annotate a query sequence against a set of reference sequences considering indels
+def alignAnnotateEachSeqMMindels(querySeq, refSeqsDict, startPos, refPosDict):
+
+    # Make a dictionary of alignment results against all references 
+    alignmentsDict = {refSeqID: seqlib.alignEmbossNeedle(querySeq, refSeq) for refSeqID, refSeq in refSeqsDict.items()}
+
+    # Make a dictionary of HD of all the aligned query-reference pairs
+    hamDict = {refSeqID: seqlib.hamming(alignedQuery, alignedRef) for refSeqID, [alignedQuery, alignedRef] in alignmentsDict.items()}
+
+    # Find the references (ID-seq pairs) that have the smallest hamming distance (i.e. the most similar)
+    minHam = min(hamDict.values())
+    minHamAlignmentIDs = [refSeqID for refSeqID, refSeqHam in hamDict.items() if refSeqHam == minHam]
+
+    # Output warning if more than one reference has the min hamming distance
+    if len(minHamAlignmentIDs) > 1:
+        print "Warning! Multiple aligned reference sequences have the same Hamming distance with the aligned query sequence"
+
+    # Get list of mismatches and indels
+    matchID = minHamAlignmentIDs[0]
+    listMM, listIn, listDel = seqlib.findMismatchesAndIndels(alignmentsDict[matchID][0], alignmentsDict[matchID][1], startPos, refPosDict[matchID])
+    return makeAnnotation(matchID, len(listMM), len(listIn), len(listDel), listMM, listIn, listDel)
+
+    
+## Align and annotate a query sequence against a set of reference sequences
+def alignAnnotateEachSeq(querySeq, refSeqsDict, startPos, refPosDict, MMcutoff, indelMode):
+    
+    # Check if perfect match with any of the reference sequences
+    for refSeqID, refSeq in refSeqsDict.items():
+        if querySeq == refSeq:
+            return makeAnnotation(refSeqID, 0, 0, 0, [], [], [])
+
+    # Look for mismatches only no-indel mode:
+    if not indelMode:
+        if not MMcutoff:
+            MMcutoff = len(querySeq)
+        return alignAnnotateEachSeqMMonly(querySeq, refSeqsDict, startPos, refPosDict, MMcutoff)
+
+    # Look for mismatches and indels in indel mode:
+    else:
+        return alignAnnotateEachSeqMMindels(querySeq, refSeqsDict, startPos, refPosDict)
 
 
 def main():
 
     ## Get options and arguments from command line
     parser = argparse.ArgumentParser(description="align and annotate a given list of sequences against a set of reference sequences")
-    parser.add_argument('-i', '--indel', action='store_true', help="enable alignment and annotation of indel sequences")
-    parser.add_argument('refSeqFilePath', help="path to the reference sequences file (in FASTQ format)")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-i', '--indel', action='store_true', help="indel mode: enable alignment and annotation of sequences with indels (default=false)")
+    group.add_argument('-m', '--MMcutoff', type=int, help="indicate the maximum number of mismatches allowed in the no-indel mode (default=length of the query sequence)")
+    parser.add_argument('-q', '--numberedMode', action='store_true', help="enable non-sequential numbering of the nucleotides along the sequence using the quality score (default=false)")
+    parser.add_argument('-s', '--startPos', type=int, default=1, help="number indictating the position of the first base of the read (default=1)")
+    parser.add_argument('-n', '--numCore', type=int, default=1, help="number of cores to use (default=1)") 
+    parser.add_argument('refSeqFilePath', help="path to the reference sequences file (in FASTA/FASTQ format)")
     parser.add_argument('seqCol', help="column of the file containing the list of sequences to be aligned and annotated (in Python notation)")
     parser.add_argument('seqFilePath', help="path to the file containing the list of sequences to be aligned and annotated")
-    parser.add_argument('outCol', help="column of the output file to which the annotation will be written to")
+    parser.add_argument('outCol', help="column of the output file to which the annotation will be written to (in Python notation)")
     parser.add_argument('outputFilePath', help="path to the output file")
     args = parser.parse_args()
 
 
     ## Initialization
-
+    phredOffset = 15
     
-
-
+    ## Read reference sequences (and numbering of nucleotides if in numberedMode)
+    refSeqsDict = {}
+    refPosDict = {}
+    if args.numberedMode:
+        for record in SeqIO.parse(args.refSeqFilePath, 'fastq'):
+            refSeqsDict[record.id] = str(record.seq.upper())
+            refPosDict[record.id] = [i-phredOffset for i in record.letter_annotations['phred_quality']]
+    else:
+        for record in SeqIO.parse(args.refSeqFilePath, 'fasta'):
+            refSeqsDict[record.id] = str(record.seq.upper())
+            refPosDict[record.id] = []
+        
+    ## Load seqfile
+    allSeqs = pd.read_csv(args.seqFilePath, sep='\t', header=None)    
     
-
-
-
+    #> multiprocessing
+    for seq in allSeqs[int(args.seqCol)]:
+        print alignAnnotateEachSeq(seq.upper(), refSeqsDict, args.startPos, refPosDict, args.MMcutoff, args.indel)
 
     
     return 1 
