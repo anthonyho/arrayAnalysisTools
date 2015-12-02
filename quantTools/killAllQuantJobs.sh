d85f36cf6be396a4e063c6fd119b9ef3b4909fd9
Anthony Ho <anthony.hyho@gmail.com>
2015-12-02 13:59:02 -0800

    Fix bugs in killAllQuantJobs.sh: wrong conditions in if loop, kill variable syntax, etc

--- quantTools/killAllQuantJobs.sh
+++ quantTools/killAllQuantJobs.sh
@@ -3,7 +3,7 @@
 # Bash script to kill all the quantification jobs running under my name all at once
 #
 # Anthony Ho, ahho@stanford.edu, 1/15/2015
-# Last update 1/15/20015
+# Last update 12/2/20015
 
 user=$(whoami)
 
@@ -12,21 +12,34 @@ case ${answer:0:1} in
     y|Y )
         # Kill the parent quantifyAllTiles.sh script
 	bash_jobs=$(ps aux | grep -E "$user.*[b]ash.*quantifyAllTiles" | awk '{print $2}')
-	if [ -z "$bash_jobs" ]
+	echo "Found bash jobs" $bash_jobs
+	if [ ! -z "$bash_jobs" ]
 	then
+	    echo "Killing bash jobs" $bash_jobs "now..."
 	    kill $bash_jobs
 	fi
 	# Kill the quantifyAllTiles.py script
 	python_jobs=$(ps aux | grep -E "$user.*[p]ython.*quantifyAllTiles" | awk '{print $2}')
-	if [ -z "$python_jobs" ]
+	echo "Found python jobs" $python_jobs
+	if [ ! -z "$python_jobs" ]
 	then
+	    echo "Killing python jobs" $python_jobs "now..."
 	    kill $python_jobs
 	fi
 	# Kill the MATLAB instances
-	matlab_jobs=$(ps aux | grep -E "$user.*[m]atlab.*AnalyseImage" | awk '{print $2}')
-	if [ -z "$matlab_jobs" ]
+	matlab_regJobs=$(ps aux | grep -E "$user.*[m]atlab.*GenerateRegistrationOffsetMap" | awk '{print $2}')
+	echo "Found matlab registration jobs" $matlab_regJobs
+	if [ ! -z "$matlab_regJobs" ]
 	then
-	    kill $matlab_jobs
+	    echo "Killing matlab registration jobs" $matlab_regJobs "now..."
+	    kill $matlab_regJobs
+	fi
+	matlab_quantJobs=$(ps aux | grep -E "$user.*[m]atlab.*AnalyseImage" | awk '{print $2}')
+	echo "Found matlab quantification jobs" $matlab_quantJobs
+	if [ ! -z "$matlab_quantJobs" ]
+	then
+	    echo "Killing matlab quantification jobs" $matlab_quantJobs "now..."
+	    kill $matlab_quantJobs
 	fi
     ;;
     * )
