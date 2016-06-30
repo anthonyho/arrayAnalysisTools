d6622ec9e01bdd36f13fbe672f1e83d0ab192d04
Anthony Ho <anthony.hyho@gmail.com>
2016-06-29 17:07:12 -0700

    add example scripts

--- quantTools/exampleWrapperScripts/01_preprocessImageSeries.sh
+++ quantTools/exampleWrapperScripts/01_preprocessImageSeries.sh
@@ -4,20 +4,22 @@
 #   1. Assign relative symbolic links to images into the appropriate time series image directories
 #   2. Rename image file prefix (e.g. "tile") if necessary
 #
-# Anthony Ho, ahho@stanford.edu, 6/28/2015
+# Anthony Ho, ahho@stanford.edu, 6/28/2016
 
 
 ## Define input and output parameters
-image_dir="$HOME/data/imaging/20160609_CNv3_3_ANUUN_01/05_sm_kd_1"
+input_dir=$1
+exp_dir=$2
+image_dir=$HOME/"data/imaging"/$input_dir/$exp_dir
 image_files_suffix="_green_2*_500ms_*.tif"
 
-output_dir="images"
-output_subdir_prefix="s"
+output_dir=$HOME/"analysis/imaging/20160609_CNv3_3_ANUUN_01"/$exp_dir/"images"
+output_subdir_prefix="t"
 output_file_prefix="tile"
 
-num_timepoints=19
+num_timepoints=$3
 tiles=$(echo {1..15})
 
 
 ## Run
-$HOME/scripts/arrayAnalysisTools/quantTools/preprocessImageSeries.sh $image_dir $image_files_suffix $output_dir $output_subdir_prefix $output_file_prefix $num_timepoints "$tiles" 
+$HOME/scripts/arrayAnalysisTools/quantTools/preprocessImageSeries.sh $image_dir $image_files_suffix $output_dir $output_subdir_prefix $output_file_prefix $num_timepoints "$tiles"
