04cb317afe2f971efb10e7812f921209901b43ab
Anthony Ho <anthony.hyho@gmail.com>
2016-08-09 17:42:33 -0700

    add df capacity to parallel apply

--- pythonLib/parlib.py
+++ pythonLib/parlib.py
@@ -1,6 +1,6 @@
 # Anthony Ho, ahho@stanford.edu, 8/4/2016
 # Last update 8/9/2016
-"""Library functions for embarrassingly parallelization of large lists, Pandas series and dataframes"""
+"""Library functions for embarrassingly parallelization of large lists, Pandas series and dataframes (by row)"""
 
 
 import numpy as np
@@ -11,28 +11,36 @@ import multiprocessing
 
 
 #
-def _splitIntoChunks(listToSplit, numCores):
+def _splitIntoChunks(listToSplit, numChunks):
     lenList = len(listToSplit)
-    chunkSize = max(1, lenList // numCores)
+    chunkSize = max(1, lenList // numChunks)
     return [listToSplit[i:i + chunkSize] for i in range(0, lenList, chunkSize)]
 
 
 #
-def _funcChunkSeries(dataChunk, func):
-    return pd.Series([func(row) for row in dataChunk], index=dataChunk.index)
-    
+def _funcChunkSeries(dataChunk, func, *args, **kwargs):
+    return dataChunk.apply(func)
+
+
+#
+def _funcChunkDf(dataChunk, func, *args, **kwargs):
+    return dataChunk.apply(func, axis=1)
+
 
 # 
 def parallelApply(data, func, numCores, verbose=0, *args, **kwargs):
-    '''Parallelizes apply to a large list, Pandas series, or dataframe'''
+    '''Parallelizes apply to a large list, Pandas series, or dataframe (row)'''
     if isinstance(data, list):
         results = None
     elif isinstance(data, pd.Series):
         listIndicesChunks = _splitIntoChunks(data.index, numCores)
         listDataChunks = [data.loc[indicesChunk] for indicesChunk in listIndicesChunks]
-        listResultsChunks = Parallel(n_jobs=numCores, verbose=verbose)(delayed(_funcChunkSeries)(dataChunk, func) for dataChunk in listDataChunks)
+        listResultsChunks = Parallel(n_jobs=numCores, verbose=verbose)(delayed(_funcChunkSeries)(dataChunk, func, *args, **kwargs) for dataChunk in listDataChunks)
         results = pd.concat(listResultsChunks)
     elif isinstance(data, pd.DataFrame):
-        results = None
-    
+        listIndicesChunks = _splitIntoChunks(data.index, numCores)
+        listDataChunks = [data.loc[indicesChunk] for indicesChunk in listIndicesChunks]
+        listResultsChunks = Parallel(n_jobs=numCores, verbose=verbose)(delayed(_funcChunkDf)(dataChunk, func, *args, **kwargs) for dataChunk in listDataChunks)
+        results = pd.concat(listResultsChunks)
+
     return results
