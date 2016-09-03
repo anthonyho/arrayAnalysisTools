7a68b1d9ed4a013e824c4933da289194778275de
Anthony Ho <anthony.hyho@gmail.com>
2016-09-03 14:23:09 -0700

    add plotBindingCurvesAcrossTargets()

--- projectLib/CN_plotlib.py
+++ projectLib/CN_plotlib.py
@@ -16,7 +16,7 @@ from fittinglibs import plotting, processresults
 figureDir = 'figures/'
 
 
-# Plot fitted binding curve single variant
+# Plot fitted binding curve of a single variant
 # Borrowed from Sarah's pipeline with modifications to show Kd instead of dG, 
 # and allow for using current axis
 def plotBindingCurve(affinityData, variant, annotate=True, ax=None):
@@ -56,5 +56,43 @@ def plotBindingCurve(affinityData, variant, annotate=True, ax=None):
                           ]
         ax.annotate('\n'.join(annotationText), xy=(0.05, .95), xycoords='axes fraction',
                     horizontalalignment='left', verticalalignment='top', fontsize=11)
+
+    return ax
+
+
+# Plot fitted binding curves of a single variant across different targets
+# Borrowed from Sarah's pipeline with modifications to show Kd instead of dG, 
+# and allow for using current axis
+def plotBindingCurvesAcrossTargets(affinityDataList, variant, 
+                                  colors=None, names=None, ax=None):
+    '''Plot a binding curve of a particular variant'''
+
+    if colors is None:
+        colors = sns.color_palette("Paired", 12)
+
+    if ax is None:
+        fig = plt.figure(figsize=(4, 4))
+        ax = fig.add_subplot(111)
+
+    for i, affinityData in enumerate(affinityDataList):
+        
+        # Load data
+        subSeries = affinityData.getVariantBindingSeries(variant)
+        if len(subSeries)==0:
+            print 'No fluorescence data associated with variant %s'%str(variant)
+            return
+        concentrations = affinityData.x
+        variant_table = affinityData.variant_table
+        
+        # Plot
+        plotting.plotFitCurve(concentrations, subSeries, variant_table.loc[variant], ax=ax)
+        
+        ax.lines[i*4+3].set_color(colors[i])
+        if names is not None:
+            ax.lines[i*4+3].set_label(names[i])
+    
+    plotlib.setproperties(xlabel='Concentration (nM)', ylabel='Fluorescence', 
+                          labelfontsize=16, tickfontsize=16, 
+                          legend=names, legendloc=2, legendfontsize=12)
 
     return ax
