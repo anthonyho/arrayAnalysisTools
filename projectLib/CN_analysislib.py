9dd33f3d2ff12867f7fa346d9a1076ba0c5e0040
Anthony Ho <anthony.hyho@gmail.com>
2016-09-04 14:14:36 -0700

    add function to generate library sequences

--- projectLib/CN_analysislib.py
+++ projectLib/CN_analysislib.py
@@ -5,6 +5,41 @@
 
 import numpy as np
 import pandas as pd
+import varlib
+
+
+# Generate library sequences
+def generateLibSeq(sublib='all', form='dict'):
+    
+    # Define names and sequences
+    LP5a_refSeq = "NNGGATTTTCCNNNACGAAGTGTCCCGAGA"
+    LP5b_refSeq = "NNGGATTTTCCNNACGAAGTNGTCCCGAGA"
+    LP6_refSeq =  "NNGGATTTTCCNNNACGAAGTNGTCCCGAG"
+    LP7a_refSeq = "NNGGATTTTCCNNNNACGAAGTNGTCCCGA"
+    LP7b_refSeq = "NNGGATTTTCCNNNACGAAGTNNGTCCCGA"
+    LP8_refSeq =  "NNGGATTTTCCNNNNACGAAGTNNGTCCCG"
+    allRefSeqs = [LP5a_refSeq, LP5b_refSeq, LP6_refSeq, LP7a_refSeq, LP7b_refSeq, LP8_refSeq]
+    libNames = ['LP5a', 'LP5b', 'LP6', 'LP7a', 'LP7b', 'LP8']
+
+    # Generate library sequences
+    libSeqs_dict = {}
+    for name, refSeq in zip(libNames, allRefSeqs):
+        libSeqs_dict[name] = varlib.generateSatLib(refSeq)
+
+    # Return the appropiate sub library or form
+    if sublib != 'all':
+        return libSeqs_dict[sublib]
+    else:
+        if form == 'dict':
+            return libSeqs_dict
+        elif form == 'list':
+            return [seq for name in libNames for seq in libSeqs_dict[name]]
+        elif form == 'df':
+            listAllLibSeqs = [seq for name in libNames for seq in libSeqs_dict[name]]
+            listAllLibNames = []
+            for name in libNames:
+                listAllLibNames = listAllLibNames + [name] * len(libSeqs_dict[name])
+            return pd.DataFrame({'lib': listAllLibNames, 'seq': listAllLibSeqs})
 
 
 # Filter variants
