789f0ddad102f9cd517c0628b042ae815d810075
Anthony Ho <anthony.hyho@gmail.com>
2017-01-09 14:21:28 -0800

    add ci to mergeAllVariants()

--- projectLib/noselibs/process.py
+++ projectLib/noselibs/process.py
@@ -18,15 +18,26 @@ def mergeAllVariants(variants_dict, variants_max_dict, bindingSeries_dict, conce
     # Compute median signals of binding series for each variant, and merge dict of binding series into a single multilabel dataframe
     medianBindingSeriesByVariants_dict = {}
     medianNormBindingSeriesByVariants_dict = {}
+    ciBindingSeriesByVariants_dict = {}
+    ciNormBindingSeriesByVariants_dict = {}
     for currSM in listSM:
         medianBindingSeriesByVariants = pd.merge(bindingSeries_dict[currSM], annotatedClusters,
                                                  how='inner', left_index=True, right_index=True).groupby('variant_number').median()
         medianBindingSeriesByVariants.columns = ['bs_'+str(c) for c in concentrations_dict[currSM]/1000]
+        medianBindingSeriesByVariants_dict[currSM] = medianBindingSeriesByVariants
+
         medianNormBindingSeriesByVariants = medianBindingSeriesByVariants.divide(variants_max[currSM]['max'], axis=0)
         medianNormBindingSeriesByVariants.columns = [col+'_norm' for col in medianNormBindingSeriesByVariants.columns]
-        
-        medianBindingSeriesByVariants_dict[currSM] = medianBindingSeriesByVariants
         medianNormBindingSeriesByVariants_dict[currSM] = medianNormBindingSeriesByVariants
+
+        ciBindingSeriesByVariants = pd.merge(bindingSeries_dict[currSM], annotatedClusters,
+                                             how='inner', left_index=True, right_index=True).groupby('variant_number').aggregate(lambda x: np.std(x) / x.count() * 1.96))
+        ciBindingSeriesByVariants.columns = ['ci_bs_'+str(c) for c in concentrations_dict[currSM]/1000]
+        ciBindingSeriesByVariants_dict[currSM] = ciBindingSeriesByVariants
+
+        ciNormBindingSeriesByVariants = ciBindingSeriesByVariants.divide(variants_max[currSM]['max'], axis=0)
+        ciNormBindingSeriesByVariants.columns = [col+'_norm' for col in ciNormBindingSeriesByVariants.columns]
+        ciNormBindingSeriesByVariants_dict[currSM] = ciNormBindingSeriesByVariants
     
     variants_bindingSeries = pd.concat(medianBindingSeriesByVariants_dict, axis=1)
     variants_normBindingSeries = pd.concat(medianNormBindingSeriesByVariants_dict, axis=1)
