50c406461a215835e39f453b1bfa58ce2ca4661b
Anthony Ho <anthony.hyho@gmail.com>
2017-01-09 15:21:37 -0800

    fix bugs

--- projectLib/noselibs/process.py
+++ projectLib/noselibs/process.py
@@ -43,9 +43,13 @@ def mergeAllVariants(variants_dict, variants_max_dict, bindingSeries_dict, conce
     
     variants_bindingSeries = pd.concat(medianBindingSeriesByVariants_dict, axis=1)
     variants_normBindingSeries = pd.concat(medianNormBindingSeriesByVariants_dict, axis=1)
+    variants_ciBindingSeries = pd.concat(ciBindingSeriesByVariants_dict, axis=1)
+    variants_ciNormBindingSeries = pd.concat(ciNormBindingSeriesByVariants_dict, axis=1)
 
     # Merge variants_dict, variants_max_dict, and bindingSeries into a single multilabel dataframe
-    variants_all = pd.concat([variants, variants_max, variants_bindingSeries, variants_normBindingSeries], axis=1).sort_index(axis=1)
+    variants_all = pd.concat([variants, variants_max, 
+                              variants_bindingSeries, variants_normBindingSeries, 
+                              variants_ciBindingSeries, ciNormBindingSeries], axis=1).sort_index(axis=1)
 
     # Compute new columns
     variants_all_swapped = variants_all.swaplevel(0, 1, axis=1).sort_index(axis=1)
