3a995eff2d23ec9250696b8a64e688fada623f5b
Anthony Ho <anthony.hyho@gmail.com>
2017-01-10 11:29:38 -0800

    add library of plotting functions

---
+++ projectLib/noselibs/plot.py
@@ -0,0 +1,33 @@
+# Anthony Ho, ahho@stanford.edu, 1/10/2016
+# Last update 1/10/2015
+"""Library containing plotting functions"""
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotlib
+
+
+def plotPredictedConcMatrix(predictedConcMatrix, setup, fig_dir, nAptamers=None, cat_colors=None, vmax=350, figsize=(8, 7)):
+    plt.figure(figsize=(8, 7))
+    cg = sns.clustermap(predictedConcMatrix.transpose(),
+                        row_colors=cat_colors, col_colors=cat_colors,
+                        row_cluster=False, col_cluster=False, vmax=vmax, vmin=0,
+                        cbar_kws={'label': 'Predicted concentration (uM)'})
+    cax = plt.gcf().axes[-1]
+    cax.set_position([0.13, .2, .03, .45])
+    if cat_colors is None:
+        title_y = 1.05
+    else: 
+        title_y = 1.1
+    if nAptamers is None:
+        cg.ax_heatmap.set_title(setup, y=title_y)
+    else:
+        cg.ax_heatmap.set_title(setup+', n='+str(nAptamers), y=title_y)
+    plotlib.setproperties(ax=cg.ax_heatmap, tight=False, 
+                          xlabel='Predicted as ...', ylabel='Pure sample of ...',
+                          fontsize=18, yticklabelrot=0)
+    plotlib.setproperties(ax=cax, tight=False, fontsize=18, yticklabelrot=0)
+    cg.savefig(fig_dir+'/conMat_allXall_'+setup+'.png')
+    cg.savefig(fig_dir+'/conMat_allXall_'+setup+'.eps')
+
+
