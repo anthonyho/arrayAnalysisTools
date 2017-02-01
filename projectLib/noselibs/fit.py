ab82cdc27a9f43bc0f25b72b21983ed9964e2d68
Anthony Ho <anthony.hyho@gmail.com>
2017-01-31 18:24:48 -0800

    fix typecasting problem with np array vs pd.DataFrame

--- projectLib/noselibs/fit.py
+++ projectLib/noselibs/fit.py
@@ -1,5 +1,5 @@
 # Anthony Ho, ahho@stanford.edu, 1/5/2017
-# Last update 1/30/2017
+# Last update 1/31/2017
 """Library containing functions for fitting and related higher level analysis"""
 
 
@@ -207,16 +207,25 @@ def reportFitStatusAllSamples(dict_fitResults):
 
 
 def evaluatePerformance(y1, y2, metric='RMSLE', axis=0):
+    # Typecast into Pandas dataframes
+    _y1 = pd.DataFrame(y1)
+    _y2 = pd.DataFrame(y2)
+
     if metric == 'RMSE':
-        return np.sqrt(((y1 - y2)**2).mean(axis=axis))
+        RMSE = np.sqrt(((_y1 - _y2)**2).mean(axis=axis))
+        return RMSE.dropna()
     elif metric == 'RMSLE':
-        LE = np.log(y1+1) - np.log(y2+1)
-        return np.sqrt((LE**2).mean(axis=axis))
+        LE = np.log(_y1+1) - np.log(_y2+1)
+        RMSLE = np.sqrt((LE**2).mean(axis=axis))
+        return RMSLE.dropna()
     elif metric == 'IRMSLE':
-        LE = np.log(y1+1) - np.log(y2+1)
-        return 1 / (1 + np.sqrt((LE**2).mean(axis=axis)))
+        LE = np.log(_y1+1) - np.log(_y2+1)
+        IRMSLE = 1 / (1 + np.sqrt((LE**2).mean(axis=axis)))
+        return IRMSLE.dropna()
     elif metric == 'IERMSLE':
-        LE = np.log(y1+1) - np.log(y2+1)
-        return np.exp(-np.sqrt((LE**2).mean(axis=axis)))
+        LE = np.log(_y1+1) - np.log(_y2+1)
+        IERMSLE = np.exp(-np.sqrt((LE**2).mean(axis=axis)))
+        return IERMSLE.dropna()
     elif metric == 'pearson':
-        return y1.corrwith(y2, axis=axis)
+        pearson = _y1.corrwith(_y2, axis=axis)
+        return pearson.dropna()
